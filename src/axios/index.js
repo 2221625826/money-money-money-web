67cafba94d059a1af13c5e5f1d00564fa91de256
zyh <2221625826@qq.com>
2022-08-23 21:58:40 +0800

    online bug

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-if (process.env.NODE_ENV == 'development') {
-    axios.defaults.baseURL = '/api' //此路径为配置代理服务器时的代理路径
-} else if (process.env.NODE_ENV == 'production') {
-    //todo
-}
+axios.defaults.baseURL = '/api' //此路径为配置代理服务器时的代理路径
 
 axios.interceptors.request.use(config => {
     // 表示在配置中的设置头消息的字段Authorization为从本地获取的token值
