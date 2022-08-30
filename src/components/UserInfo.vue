c4d1999ee74df42db4443ede76270a1c44b378a3
zhangyiheng03 <zhangyiheng03@corp.netease.com>
2022-08-30 16:38:43 +0800

    add: userInfo

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserInfo',
+    props: {
+      msg: String
+    },
+    data() {
+      return { 
+        userInfo:{}
+      }
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  
+  </style>
+  
---
+++ src/views/UserInfoView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <UserInfo/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import UserInfo from '@/components/UserInfo.vue'
+  
+  export default {
+    name: 'UserInfoView',
+    components: {
+        UserInfo
+    }
+  }
+  </script>
+  
